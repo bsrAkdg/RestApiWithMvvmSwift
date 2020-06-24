fcde14ffe5797ce434f608cc0a355776e6ef552a
Büşra Serdaroğlu <bserdaroglu@thy.com>
2020-06-24 22:34:34 +0300

    Project create

---
+++ CryptoCrazy/CryptoCrazy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCrazy
+//
+//  Created by Büşra Serdaroğlu on 24.06.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
